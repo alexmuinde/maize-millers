534af3851e33c36bad9367f4739a700d87ba57e5
alexmuinde <alexmuinde470@gmail.com>
2023-11-28 05:06:22 -0800

    Add redux toolkit

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -19,7 +19,7 @@ export default function Header(){
                 <ul className='flex gap-4'>
                     <Link to='/'><li className='hidden sm:inline text-slate-700 hover:underline'>Home</li></Link>
                     <Link to='/about'><li className='hidden sm:inline text-slate-700 hover:underline'>About</li></Link>
-                    <Link to='/sign-in'><li className='hover:underline'>SignIn</li></Link>
+                    <Link to='/sign-in'><li className='hover:underline'>Sign In</li></Link>
                 </ul>
             </div>
         </header>
